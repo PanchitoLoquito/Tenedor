754ea1d22f4d4ef1c7ea2ca045af5e24f8ebc47b
PanchitoLoquito <mr.cookie.f@gmail.com>
2026-04-19 16:25:21 -0300

    Add files via upload

---
+++ TrabajoPractico3/Tenedor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico3
+{
+    internal class Tenedor
+    {
+        int vida = 50;
+        int velocidad;
+        int position; //lo hago un entero pq no existe Vector2 en C# base
+        int daño;
+        int sala;
+
+        public Tenedor(int position)
+        {
+            this.position = position;
+        }
+
+        public void Muerte()
+        {
+            //quequefree(); al no ser una entidad no se puede eliminar
+            Console.WriteLine("Muerto");
+        }
+
+        public void Ataque(int vidajugador)
+        {
+            vidajugador = vidajugador - daño;
+        }
+
+        public void RecibirDaño(int dañojugador)
+        {
+            vida = vida - dañojugador;
+        }
+        
+        public void SeguirJugador(int salajugador)
+        {
+            if (salajugador == sala)
+            {
+                Console.WriteLine("Tenedor comenzo a seguirte");
+            }
+        }
+
+        public int ObtenerVida()
+        {
+            return vida;
+        }
+
+        public int ObtenerSala()
+        {
+            return sala;
+        }
+    }
+}
---
+++ TrabajoPractico3/Tester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPractico3
+{
+    internal class Tester
+    {
+        static void Main(string[] args)
+        {
+            Tenedor tenedor = new Tenedor(5);
+
+            Console.WriteLine("=== TEST TENEDOR ===");
+
+            Console.WriteLine("Vida inicial: " + tenedor.ObtenerVida());
+
+            tenedor.RecibirDaño(50);
+            Console.WriteLine("Vida despues de recibir daño: " + tenedor.ObtenerVida());
+
+            int vidaJugador = 100;
+            tenedor.Ataque(vidaJugador);
+            Console.WriteLine("Vida jugador despues de ataque: " + vidaJugador);
+
+            Console.WriteLine("Probando seguir jugador...");
+            tenedor.SeguirJugador(0);
+
+            Console.WriteLine("Forzando muerte...");
+            tenedor.Muerte();
+
+            Console.WriteLine("=== FIN TEST ===");
+        }
+    }
+    
+}
